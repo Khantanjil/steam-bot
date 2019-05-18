1d2a8df4b8d8be9c94957d3b5c3d3d87968bc502
Takken <47666341+TAKKEN666@users.noreply.github.com>
2019-05-18 11:47:09 +0100

    Add files via upload

---
+++ javascriptsteam.js
@@ -0,0 +1,17 @@
+const SteamUser = require('steam-user');
+const config = require('./config.json');
+const client = new SteamUser ();
+
+const logInOptions = {
+	accountName: config.username,
+	password: config.password,
+
+};
+client.logOn(logInOptions)
+
+client.on ('loggedOn', () => {
+	console.log('yes');
+	client.setPersona(SteamUser.EPersonaState.Offline);
+	client.gamesPlayed(730);
+});
+
